8e81f5e8565e2eb39447b5808750c0039c0eadbe
AnteKoz <Ante.Kozina@adesso.de>
2024-06-26 08:15:21 +0200

    feat: Adding table of Frontend

--- car-frontend/src/App.tsx
+++ car-frontend/src/App.tsx
@@ -1,5 +1,7 @@
 
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useEffect, useState } from 'react';
+import * as React from 'react';
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -16,16 +18,33 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-        <ul>
-          {data.map((car: any, index) => (
-                        <tr key={index}>
-                        <td>{car.brand}</td>
-                        <td>{car.code}</td>
-                    </tr>
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Brands</TableCell>
+            <TableCell align="right">Type</TableCell>
+            <TableCell align="right">Model</TableCell>
+            <TableCell align="right">Code</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row: any) => (
+            <TableRow
+              key={row.brand}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.brand}
+              </TableCell>
+              <TableCell align="right">{row.type}</TableCell>
+              <TableCell align="right">{row.model}</TableCell>
+              <TableCell align="right">{row.code}</TableCell>
+            </TableRow>
           ))}
-        </ul>
-    </div>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 export default App;
